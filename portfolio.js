8a97b3bd9ac6768b965627ad14af7ea1e63f3f08
Rachel McCardell <rachelmccardell@Rachels-MBP.attlocal.net>
2020-06-05 17:25:00 -0400

    Finished

--- portfolio.js
+++ portfolio.js
@@ -11,22 +11,27 @@ function typeWriter() {
 }
 
 function showTextRunning() {
+    document.getElementById("me-info").innerHTML = "";
     document.getElementById("me-info").innerHTML = "I am a student athlete on Northwestern's D1 Women's Cross Country and Track teams. Running has always been a strong passion of mine.";
 }
 
 function showTextEducation() {
-    document.getElementById("me-info").innerHTML = " I am pursuing a BS in Computer Science at Northwestern University.";
+    document.getElementById("me-info").innerHTML = "";
+    document.getElementById("me-info").innerHTML = "I am pursuing a BS in Computer Science at Northwestern University.";
 }
 
 function showTextMusic() {
+    document.getElementById("me-info").innerHTML = "";
     document.getElementById("me-info").innerHTML = "In my free time, I perform with my collegiate a cappella group, Extreme Measures, in gigs and shows around the Chicago area. Check us out on Spotify or Apple Music!";
 }
 
 function showTextHouse() {
+    document.getElementById("me-info").innerHTML = "";
     document.getElementById("me-info").innerHTML = "Detroit is where I call home. I was born and raised in SE Michigan.";
 }
 
 function showTextCoffee() {
+    document.getElementById("me-info").innerHTML = "";
     document.getElementById("me-info").innerHTML = "I am a severe coffee addict!";
 }
 
